7971368d9230b4001fb144b284663c5ac3b02721
brian-ambani <brainambani1@gmail.com>
2023-10-05 12:42:53 +0300

    updated the findUser route

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,36 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  { firstName: "John", lastName: "Doe", age: 25 },
-  { firsName: "Jane", lastName: "Doe", age: 24 },
-];
+const users = [];
 
+//getting all users
 router.get("/", (req, res) => {
-  console.log(users);
   res.send(users);
 });
 
+//adding a user
 router.post("/", (req, res) => {
-  console.log("POST ROUTE REACHED");
+  const user = req.body;
+
+  const userId = uuidv4();
+
+  const userWithId = { ...user, id: userId };
+
+  users.push(userWithId);
+
+  res.send(`user with the name ${user.firstName} added to the database`);
 
   res.send("POST ROUTE REACHED");
+});
+
+//get a single user
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
 });
 
 export default router;
